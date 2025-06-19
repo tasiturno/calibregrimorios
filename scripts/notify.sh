3e8716787ddc8ffe6f49e347040fe0db8dab1311
Ruy <dakvin4@gmail.com>
2025-06-19 23:04:56 +0100

    Script para notificaciones vía Telegram

--- scripts/notify.sh
+++ scripts/notify.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-MENSAJE="$1"
+# Token del bot de Telegram
 TOKEN="7962768216:AAHjvAH_7hcOKMSGWqhzl6Zfif-kIbN22II"
+
+# Chat ID del destinatario
 CHAT_ID="7561699463"
 
+# Mensaje a enviar
+MESSAGE=$1
+
+# Enviar mensaje vía Telegram
 curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" \
-    -d chat_id="$CHAT_ID" \
-    -d text="$MENSAJE"
+     -d chat_id="$CHAT_ID" \
+     -d text="$MESSAGE"
